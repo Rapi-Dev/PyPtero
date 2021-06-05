07a96697968f1c13d152c9e01a991824cf23bf2a
Daftscientist <67394753+Daftscientist@users.noreply.github.com>
2021-06-05 13:29:50 +0100

    Update users.py

--- PyPtero/Sync/client/users.py
+++ PyPtero/Sync/client/users.py
@@ -1,4 +1,4 @@
 class Users:
     def __init__(self, pterodactyl: object) -> None:
         self.domain = pterodactyl.domain
-        self.api_key = pterodactyl.api_key ## test fuxkchshd
+        self.api_key = pterodactyl.api_key ## test
