b000dc3961cf3dab8d58ed3c0c0a679d1f1365fc
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-06-06 10:44:53 +0100

    Delete PyPtero/Sync/client directory

--- PyPtero/Sync/client/server.py
+++
@@ -1,4 +0,0 @@
-class Server(object):
-    def __init__(self, pterodactyl: object) -> None:
-        self.domain = pterodactyl.domain
-        self.api_key = pterodactyl.api_key
--- PyPtero/Sync/client/users.py
+++
@@ -1,4 +0,0 @@
-class Users:
-    def __init__(self, pterodactyl: object) -> None:
-        self.domain = pterodactyl.domain
-        self.api_key = pterodactyl.api_key
