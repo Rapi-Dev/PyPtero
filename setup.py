b07c3b5e207e3c87aaa859c20761b658bc0895a6
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-06-17 17:00:41 +0100

    Allow pip installation

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 2 - Pre-Alpha',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='PyPtero',
+  version='0.0.1a',
+  description='A simple, advanced API wrapper for the Pterodactyl API',
+  long_description=open('README.md').read(),
+  long_description_content_type = "text/markdown",
+  url = "https://github.com/Rapi-Dev/PyPtero/", 
+  project_urls={
+   "Documentation": "https://github.com/Rapi-Dev/PyPtero/docs/",
+   "Issues tracker": "https://github.com/Rapi-Dev/PyPtero/issues",
+   },
+  author='Seniatical',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='API-Wrapper,Pterodactyl,Async,Sync', 
+  packages=find_packages(),
+  install_requires=['requests', 'pydantic', 'aiohttp'] 
+)
